586e544923096526f9d715358f31d9a658ac4f1a
Souza <aninha.clara030807@gmail.com>
2024-03-26 10:17:09 -0300

    alteraçoes do dia 26-03

--- Aula 2/js/script.js
+++ Aula 2/js/script.js
@@ -1,4 +1,16 @@
 alerta = () => {
-  alert("Olá, sou um alerta!🚨")
-}
-  
+  alert("Olá, sou um alerta!🚨");
+};
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+
+  document.getElementById("resposta").innerHTML = `<h2>Olá ${nome}, formulário
+enviado com sucesso!</h2>`;
+
+  if (nome == "") {
+    alert("Bem vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
