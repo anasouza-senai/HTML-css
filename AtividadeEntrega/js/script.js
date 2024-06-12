a7fea024609e96dcb63fa35851e0b87dc33d5662
Souza <aninha.clara030807@gmail.com>
2024-06-12 15:09:53 -0300

    entregas

--- AtividadeEntrega/js/script.js
+++ AtividadeEntrega/js/script.js
@@ -11,4 +11,9 @@ trocaTema = () => {
   const imagem = isLinght ? "./img/dark.jpg" : "./img/light.jpg";
 
   document.querySelector("#perfil img").setAttribute("src", imagem);
+
+  const trocaalt= isLinght ? "uma mulher de cabelo preto, ela esta maquiada,batom vermelho, seu olho tem um tom escuro e cara de brava" :  "Uma mulher loira, cabelos esvoaçantes, maquiada, batom forte, com traços fortes";
+
+  document.querySelector("#perfil img").setAttribute("alt", trocaalt);
+
 };
